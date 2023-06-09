87b86182d6ac9f9ba34b2315f064872be93a3583
Muhammad Nur Ichsan <69494417+muhnurichsan@users.noreply.github.com>
2023-06-09 22:14:48 +0700

    fix unit test component name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = () => {
   return (
     <nav
-      data-cy="navbar-todo"
+      data-cy="header-title"
       className="bg-primary w-full h-[105px] flex items-center"
     >
       <div className="uppercase text-white text-2xl font-bold mx-20 lg:mx-80">
